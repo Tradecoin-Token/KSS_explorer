9cac936a7f355e0c577c3673c6f350a1b963a7f6
Tradecoin <8797899+Tradecoin-Token@users.noreply.github.com>
2021-02-13 23:37:40 +0530

    Add files via upload

---
+++ src/js/configuration/config.mainnet.js
@@ -0,0 +1,10 @@
+const nodeUrl = 'http://68.183.110.222:6869';
+
+export default {
+    networkId: 'mainnet',
+    displayName: 'Mainnet',
+    apiBaseUrl: 'http://68.183.110.222:6869',
+    useCustomRequestConfig: false,
+   
+    nodes: [{url: nodeUrl, maintainer: 'KSS'}]
+};
---
+++ src/js/configuration/index.js
@@ -0,0 +1,7 @@
+import mainnet from './config.mainnet';
+
+
+const configuredNetworks = [mainnet]
+    .filter(item => __NETWORKS__.includes(item.networkId));
+
+export default configuredNetworks;
